2cf9d95e1bd6c9e885a439fb4ed63395a5c0df4c
rafael-andrade <rafa.devjp@gmail.com>
2022-05-07 02:56:13 -0300

    Prmeiro commit branch dev

--- src/main/java/com/dio/santander/bankLine/api/BankLineApiApplication.java
+++ src/main/java/com/dio/santander/bankLine/api/BankLineApiApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BankLineApiApplication {
 
 	public static void main(String[] args) {
-//		System.out.println("Hello World!!!");
+ 	System.out.println("Hello World!!!");
 		SpringApplication.run(BankLineApiApplication.class, args);
 	}
 
